92eb277547f7062265f22d6b8772f5df77ae685e
r z <r z>
2017-04-12 22:31:43 +0800

    model

---
+++ location.js
@@ -0,0 +1,18 @@
+/**
+ * Created by oosap on 2017/4/12.
+ */
+location.hash
+
+location.host
+
+location.hostname
+
+location.href
+
+location.origin
+
+location.port
+
+location.protocol
+
+location.search
