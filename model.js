686f15c9cdfdecf0ea164ccc7198444f408d20bd
OoSpace <oospace@foxmail.com>
2017-04-13 17:37:54 +0800

    Update model.js

--- model.js
+++ model.js
@@ -1,6 +1,3 @@
-/**
- * Created by oosap on 2017/4/12.
- */
 var baseModel=function () {
     this.$describle="basic model";
     this.$doName="baseModel";
@@ -8,53 +5,59 @@ var baseModel=function () {
     this.$url="";
     this.$type="get";
     this.$dataType="json";
-
+    this.$ajax=$.ajax;
 }
+
 var BMP=baseModel.prototype;
-var _setParams=["params","describle","type","url","execute"];
+var _setFnNameList=["params","describle","type","url","execute"];
+
 BMP.$addMethod=function (name,fn) {
     this["$set"+name.replace(name.substring(0,1),name.substring(0,1).toUpperCase())]=fn;
-    //console.log("==$set"+name.replace(name.substring(0,1),name.substring(0,1).toUpperCase())+"==")
-    //console.log(fn.toString())
     return this;
 }
+
 var _BMP=new baseModel();
-_BMP.$describle="_BMP";
-for(var i=0;i<_setParams.length;i++){
-    name=_setParams[i];
-    console.log(name)
+    _BMP.$describle="_BMP";
+
+for(var i=0;i<_setFnNameList.length;i++){
+
+    var name=_setFnNameList[i];
+
     _BMP.$addMethod(name,function (_name,_args){
         this.$doName=_name;
         this["$"+_name]=_args;
         console.log(_BMP["$"+_name]);
-        //console.log(_BMP);
         return this;
     })
 }
 var _param={name:"test"};
+
 _BMP.$setParams("params",_param);
 _BMP.$setDescrible("describle","test");
+
 console.log(_BMP.$params);
 console.log(_BMP.$doName);
 
 var _bmp=Object.create(_BMP);
+
+
 //console.log(_bmp.$doName);
 //console.log(_bmp.$url);
 //exports._BMP=_BMP;
 /*
- BMP.$setParams=function (params) {
- this.$params=params;
- return this;
- }
- BMP.$setDescrible=function (describle) {
- this.$describle=describle;
- return this;
- }
- BMP.$setUrl=function (url) {
- this.$url=url;
- return this;
- }
- BMP.$setType=function (type) {
- this.$type=type;
- return this;
- }*/
+BMP.$setParams=function (params) {
+    this.$params=params;
+    return this;
+}
+BMP.$setDescrible=function (describle) {
+    this.$describle=describle;
+    return this;
+}
+BMP.$setUrl=function (url) {
+    this.$url=url;
+    return this;
+}
+BMP.$setType=function (type) {
+    this.$type=type;
+    return this;
+}*/
