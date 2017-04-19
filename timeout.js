d96cb2d8ad67f03f1a3bb0a9979b8e4ed6cae0e1
OoSpace <oospace@foxmail.com>
2017-04-19 16:56:23 +0800

    Update timeout.js

--- timeout.js
+++ timeout.js
@@ -4,3 +4,18 @@ for(var i=0;i<5;i++){
     console.log(i)
   },1000)
 }
+
+
+setTimeout(function() {
+  console.log(1)
+}, 0);
+new Promise(function executor(resolve) {
+  console.log(2);
+  for( var i=0 ; i<10000 ; i++ ) {
+    i == 9999 && resolve();
+  }
+  console.log(3);
+}).then(function() {
+  console.log(4);
+});
+console.log(5);
