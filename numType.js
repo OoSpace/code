d7fe1a0d3f0b89141f482969f63156754d156c03
OoSpace <oospace@foxmail.com>
2017-04-19 17:03:38 +0800

    Create numType.js

---
+++ numType.js
@@ -0,0 +1,28 @@
+function fn() {
+    return 20;
+}
+console.log(fn + 10); // 输出结果是多少
+
+function fn() {
+    return 20;
+}
+
+fn.toString = function() {
+    return 10;
+}
+
+console.log(fn + 10);  // 输出结果是多少？
+
+function fn() {
+    return 20;
+}
+
+fn.toString = function() {
+    return 10;
+}
+
+fn.valueOf = function() {
+    return 5;
+}
+
+console.log(fn + 10); // 输出结果是多少？
