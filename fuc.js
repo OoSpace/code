144b0de63a3939bec4ea485ca73b3d4009acb699
OoSpace <oospace@foxmail.com>
2017-05-09 16:10:45 +0800

    Update fuc.js

--- fuc.js
+++ fuc.js
@@ -7,6 +7,15 @@ Foo.prototype.getName = function () { alert (3);};
 var getName = function () { alert (4);};
 function getName() { alert (5);}
 
+
+//update 2017/5/9
+
+var a=function(){};a.b=function(){console.log("c")};
+a.prototype.b=function(){console.log("d")};
+
+a.b();
+new a().b();
+
 //请写出以下输出结果：
 Foo.getName();
 getName();
